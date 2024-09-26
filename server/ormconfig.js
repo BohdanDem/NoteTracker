f06c48b87e3a90f1626ecd8b24fbda6ca254927f
BohdanDem <bogdandemchuk.1@gmail.com>
2024-09-26 11:29:38 +0300

    connect to db

---
+++ server/ormconfig.js
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+const dataSource = new DataSource({
+  name: 'default',
+  type: 'postgres',
+  host: '0.0.0.0',
+  port: '5432',
+  username: 'user',
+  password: 'password',
+  database: 'note-tracker',
+  synchronize: false,
+  migrationsRun: false,
+  migrationsTableName: 'migrations',
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/database/migrations/*{.js,.ts}'],
+});
+
+export default dataSource;
